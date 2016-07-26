f89670e3da46ba3c22db5abe87e1c62f7b468747
Aomoda <528451599@qq.com>
2016-07-26 10:36:10 +0800

    Update wenjuan.R

--- wenjuan.R
+++ wenjuan.R
@@ -4,6 +4,7 @@ library(klaR)
 library(arules)
 library(arulesViz)
 library(car)
+library(plyr)
 
 # 定义读取数据函数，并对数据重命名。
 data_get <- function(){
@@ -20,14 +21,14 @@ x <-data_get() # 读取数据
 # 分类数据，并且对数据打上标签
 data_factor <- as.data.frame(apply(X = x,MARGIN = 2,function(x) factor(x = x,levels = 1:5,labels = c('非常不需要','不需要','视情况而定','需要','非常需要'))))
 str(data_factor,list.len=5)
-
+data_factor_new <- as.data.frame(apply(X = data_num,MARGIN = 2,function(x)car::recode(x," c(-1,0)='不需要';1='视情况而定';c(2,3)='需要' ") ))
+str(data_factor_new,list.len=5)
 
 # 描述性统计分析 量化数据分析
 
 #将分类数据数值化。非常不需要=-1分，不需要=0分，视情况而定=1分，需要=2分，非常需要=3分
 data_num <- x - 2
 
-## 频率统计你做了，就用你的就行。SPSS
 
 
 ## 箱图和条形图
@@ -38,6 +39,14 @@ bar_num<-bar_num[order(bar_num,decreasing = F)]
 bar_ok <- data.frame(num=as.vector(bar_num),names=names(bar_num))
 return(bar_ok)
 }
+
+##总体分析
+par(las=2)
+barplot(bar_num(data_num)[,1],horiz = T,main='总体需求分数条形图',cex.names=0.7,names.arg=bar_num(data_num)[,2],density = 20,xlim=c(0,2))
+abline(v=0.5,lty=2,col='orange',lwd=2)
+abline(v=1,lty=2,col='red',lwd=2)
+abline(v=1.5,lty=2,col='blue',lwd=2)
+par(las=1)
 
 ###不同就餐协助方式需求程度分布
 par(mfrow=c(2,1))
@@ -68,22 +77,12 @@ par(las=2)
 barplot(bar_num(data_num[,15:19])[,1],horiz = T,main='不同精神文化与健康服务需求分数条形图',cex.names=0.7,names.arg=bar_num(data_num[,15:19])[,2],density = 20)
 par(mfrow=c(1,1),las=1)
 
-##总体分析
-par(las=2)
-barplot(bar_num(data_num)[,1],horiz = T,main='总体需求分数条形图',cex.names=0.7,names.arg=bar_num(data_num)[,2],density = 20,xlim=c(0,2))
-abline(v=0.5,lty=2,col='orange',lwd=2)
-abline(v=1,lty=2,col='red',lwd=2)
-abline(v=1.5,lty=2,col='blue',lwd=2)
-par(las=1)
+
 
 
 
 
 #apriori关联分析
-
-# 重新分类数据
-data_factor_new <- as.data.frame(apply(X = data_num,MARGIN = 2,function(x)car::recode(x," c(-1,0)='不需要';1='视情况而定';c(2,3)='需要' ") ))
-str(data_factor_new,list.len=5)
 
 
 rules <- apriori(data = data_factor_new,parameter = list(supp=0.3,conf=0.8,target='rules',minlen=2),appearance = list(rhs=c('安全呼救=需要','上门维修=需要','社区药店=需要' ),default='lhs' ))
@@ -95,6 +94,13 @@ plot(r,method ='grouped',control=list(col=gray(1:10/10)))
 plot(r,method ='matrix',control=list(col=gray(1:10/10)))
 
 
+rules_q <- apriori(data = data_factor_new,parameter = list(supp=0.3,conf=0.8,target='rules',minlen=2),appearance = list(lhs=c('安全呼救=需要','上门维修=需要','社区药店=需要' ),default='rhs' ))
+quality(rules_q) <- round(quality(rules_q),2)
+r_q<-sort(rules_q,by='lift')
+inspect(r_q)
+
+plot(r_q,method ='grouped',control=list(col=gray(1:10/10)))
+plot(r_q,method ='matrix',control=list(col=gray(1:10/10)))
 
 # k-modes 聚类分析
 
@@ -130,11 +136,35 @@ row.names=c('相异度测量值','k_modes目标函数值')
 return(round(x,0))
 }
 
-
 better_model()
+km7$size
+km7$modes
+
+
+km_plot_sum <- function() {
+four<-function(x){
+y<-data.frame(就餐方面=mean(as.numeric(x[1:3])), 医疗方面=mean(as.numeric(x[4:7])),生活方面=mean(as.numeric(x[8:14])),社区服务=mean(as.numeric(x[15:19])))
+return(y)
+}
+require(plyr)
+x <- data_num
+x$km <- km7$cluster
+km_fac<-ddply(.data = x,.variables = .(km),function(x) apply(x,2,mean))[,1:19]
+km_f<-round(rbind(four(km_fac[1,]),four(km_fac[2,]),four(km_fac[3,]),four(km_fac[4,]),four(km_fac[5,]),four(km_fac[6,]),four(km_fac[7,])),2)
+barplot(t(km_f),beside = T,horiz = T,density = c(10,20,30,40),main='kmodes分类结果对比')
+abline(v = 1,col='red',lty=2)
+abline(v = 1.5,col='blue',lty=2)
+legend('topright',legend = c('就餐方面','医疗方面','生活方面','社区服务'),density = c(10,20,30,40))
+km_f[km_f >=1.5] <- '高'
+km_f[km_f >=1 &km_f <1.5 ] <- '中'
+km_f[km_f <1] <- '低'
+return(km_f)
+}
+km_plot_sum()
+
 
 # 选择分类为7的分类
-km_plot <- function(n){
+km_plot_detail <- function(n){
 require(plyr)
 x <- data_num
 x$km <- km7$cluster
@@ -153,8 +183,8 @@ barplot(as.matrix(sort(km_fac[6,],decreasing = F) ),horiz = T,cex.names = 1,dens
 barplot(as.matrix(sort(km_fac[7,],decreasing = F) ),horiz = T,cex.names = 0.8,density = 20,main='kmodes第7分类需求分数排行')
 par(mfrow=c(1,1),las=1)}}
 
-km_plot(1)
-km_plot(2)
+km_plot_detail(1)
+km_plot_detail(2)
 
 
 
@@ -170,20 +200,9 @@ km_plot(2)
 
 
 
+#############################################################################3
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 忽略先
 # 层次聚类
 # 分类变量离散化
 dist_factor_temp <- function(data){
