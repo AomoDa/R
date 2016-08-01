9da3f2ff50d169d85e7ab4c27efc77d92b830411
Aomoda <528451599@qq.com>
2016-08-01 14:57:30 +0800

    Update wenjuan.R

--- wenjuan.R
+++ wenjuan.R
@@ -7,27 +7,31 @@ library(car)
 library(plyr)
 
 # 定义读取数据函数，并对数据重命名。
+# 定义读取数据函数，并对数据重命名。
 data_get <- function(){
 require(foreign)
+setwd('C:/Users/AomoDa/Documents')
 x <- read.spss(file = 'spss.sav',to.data.frame = T,use.value.labels = F)
 x <- data.frame(x[,-1])
 x <- as.data.frame(apply(X = x,MARGIN = 2,function(x) as.numeric(x) ))
 names(x) <- c('社区餐厅','送餐服务','上门做饭','社区药店','康复理疗','家庭医生','家庭病床','生活照料','帮助洗澡','打扫卫生','帮助购物','代办代缴','上门维修','外出接送_陪同散步','老年学校','安全呼救','结对关怀','文化娱乐','锻炼健身')
 return(x)
 }
-
 x <-data_get() # 读取数据
-
 # 分类数据，并且对数据打上标签
+
+### 原始分类数据
 data_factor <- as.data.frame(apply(X = x,MARGIN = 2,function(x) factor(x = x,levels = 1:5,labels = c('非常不需要','不需要','视情况而定','需要','非常需要'))))
 str(data_factor,list.len=5)
+
+#  量化数据分析
+#将分类数据数值化。非常不需要=-1分，不需要=0分，视情况而定=1分，需要=2分，非常需要=3分
+data_num <- x - 2
+
+### 重新分类数据
 data_factor_new <- as.data.frame(apply(X = data_num,MARGIN = 2,function(x)car::recode(x," c(-1,0)='不需要';1='视情况而定';c(2,3)='需要' ") ))
 str(data_factor_new,list.len=5)
 
-# 描述性统计分析 量化数据分析
-
-#将分类数据数值化。非常不需要=-1分，不需要=0分，视情况而定=1分，需要=2分，非常需要=3分
-data_num <- x - 2
 
 
 
@@ -94,7 +98,7 @@ plot(r,method ='grouped',control=list(col=gray(1:10/10)))
 plot(r,method ='matrix',control=list(col=gray(1:10/10)))
 
 
-rules_q <- apriori(data = data_factor_new,parameter = list(supp=0.3,conf=0.8,target='rules',minlen=2),appearance = list(lhs=c('安全呼救=需要','上门维修=需要','社区药店=需要' ),default='rhs' ))
+rules_q <- apriori(data = data_factor_new,parameter = list(supp=0.2,conf=0.6,target='rules',minlen=2),appearance = list(lhs=c('安全呼救=需要','上门维修=需要','社区药店=需要' ),default='rhs' ))
 quality(rules_q) <- round(quality(rules_q),2)
 r_q<-sort(rules_q,by='lift')
 inspect(r_q)
@@ -102,15 +106,20 @@ inspect(r_q)
 plot(r_q,method ='grouped',control=list(col=gray(1:10/10)))
 plot(r_q,method ='matrix',control=list(col=gray(1:10/10)))
 
+
+
+
 # k-modes 聚类分析
 
 set.seed(123)
+km1 <- kmodes(data = data_factor_new,modes = 1,iter.max = 10)
+km2 <- kmodes(data = data_factor_new,modes = 2,iter.max = 10)
+km3 <- kmodes(data = data_factor_new,modes = 3,iter.max = 10)
+km4 <- kmodes(data = data_factor_new,modes = 4,iter.max = 10)
 km5 <- kmodes(data = data_factor_new,modes = 5,iter.max = 10)
 km6 <- kmodes(data = data_factor_new,modes = 6,iter.max = 10)
 km7 <- kmodes(data = data_factor_new,modes = 7,iter.max = 10)
 km8 <- kmodes(data = data_factor_new,modes = 8,iter.max = 10)
-
-
 better_model <- function(){
 final_target<-function(data,model) {
 x <- data
@@ -127,6 +136,10 @@ b[i]<-sum(apply(X = xx,MARGIN = 1,FUN = d_one))
 return(sum(b))
 }
 x <- data.frame(
+km1=c(final_target(data = data_factor_new,model = km1),sum(km1$withindiff)),
+km2=c(final_target(data = data_factor_new,model = km2),sum(km2$withindiff)),
+km3=c(final_target(data = data_factor_new,model = km3),sum(km3$withindiff)),
+km4=c(final_target(data = data_factor_new,model = km4),sum(km4$withindiff)),
 km5=c(final_target(data = data_factor_new,model = km5),sum(km5$withindiff)),
 km6=c(final_target(data = data_factor_new,model = km6),sum(km6$withindiff)),
 km7=c(final_target(data = data_factor_new,model = km7),sum(km7$withindiff)),
@@ -135,9 +148,17 @@ row.names=c('相异度测量值','k_modes目标函数值')
 )
 return(round(x,0))
 }
-
 better_model()
+
+plot(as.numeric(model_choice[2,]),type='b',main='kmodes目标函数值 VS K 图')
+km5$size
+round(prop.table(km5$size),3)
+km5$modes
+
+
+
 km7$size
+round(prop.table(km7$size),3)
 km7$modes
 
 
@@ -148,9 +169,9 @@ return(y)
 }
 require(plyr)
 x <- data_num
-x$km <- km7$cluster
+x$km <- km3$cluster
 km_fac<-ddply(.data = x,.variables = .(km),function(x) apply(x,2,mean))[,1:19]
-km_f<-round(rbind(four(km_fac[1,]),four(km_fac[2,]),four(km_fac[3,]),four(km_fac[4,]),four(km_fac[5,]),four(km_fac[6,]),four(km_fac[7,])),2)
+km_f<-round(rbind(four(km_fac[1,]),four(km_fac[2,]),four(km_fac[3,])),2)
 barplot(t(km_f),beside = T,horiz = T,density = c(10,20,30,40),main='kmodes分类结果对比')
 abline(v = 1,col='red',lty=2)
 abline(v = 1.5,col='blue',lty=2)
@@ -160,31 +181,25 @@ km_f[km_f >=1 &km_f <1.5 ] <- '中'
 km_f[km_f <1] <- '低'
 return(km_f)
 }
-km_plot_sum()
 
+km_plot_sum()
 
 # 选择分类为7的分类
 km_plot_detail <- function(n){
 require(plyr)
 x <- data_num
-x$km <- km7$cluster
+x$km <- km3$cluster
 km_fac<-ddply(.data = x,.variables = .(km),function(x) apply(x,2,mean))[,1:19]
-if(n==1) {
-par(mfrow=c(2,2),las=2)
-barplot(as.matrix(sort(km_fac[1,],decreasing = F) ),horiz = T,cex.names = 0.8,density = 20,main='kmodes第1分类需求分数排行')
-barplot(as.matrix(sort(km_fac[2,],decreasing = F) ),horiz = T,cex.names = 1,density = 20,main='kmodes第2分类需求分数排行')
-barplot(as.matrix(sort(km_fac[3,],decreasing = F) ),horiz = T,cex.names = 0.8,density = 20,main='kmodes第3分类需求分数排行')
-barplot(as.matrix(sort(km_fac[4,],decreasing = F) ),horiz = T,cex.names = 1,density = 20,main='kmodes第4分类需求分数排行')
-par(mfrow=c(1,1),las=1)}
-if(n==2){
-par(mfrow=c(2,2),las=2)
-barplot(as.matrix(sort(km_fac[5,],decreasing = F) ),horiz = T,cex.names = 0.8,density = 20,main='kmodes第5分类需求分数排行')
-barplot(as.matrix(sort(km_fac[6,],decreasing = F) ),horiz = T,cex.names = 1,density = 20,main='kmodes第6分类需求分数排行')
-barplot(as.matrix(sort(km_fac[7,],decreasing = F) ),horiz = T,cex.names = 0.8,density = 20,main='kmodes第7分类需求分数排行')
-par(mfrow=c(1,1),las=1)}}
+
+if(n==1) barplot(as.matrix(sort(km_fac[1,],decreasing = F) ),horiz = T,cex.names = 0.7,density = 20,main='kmodes第1分类需求分数排行')
+if(n==2) barplot(as.matrix(sort(km_fac[2,],decreasing = F) ),horiz = T,cex.names = 0.7,density = 20,main='kmodes第2分类需求分数排行')
+if(n==3) barplot(as.matrix(sort(km_fac[3,],decreasing = F) ),horiz = T,cex.names = 0.7,density = 20,main='kmodes第3分类需求分数排行')
+
+}
 
 km_plot_detail(1)
 km_plot_detail(2)
+km_plot_detail(3)
 
 
 
