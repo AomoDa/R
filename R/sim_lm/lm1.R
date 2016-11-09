cadb2d7b73af5e97effc1cfc4c1d245de924ec9d
Aomoda <528451599@qq.com>
2016-11-09 22:10:39 +0800

    Update lm1.R

--- R/sim_lm/lm1.R
+++ R/sim_lm/lm1.R
@@ -1,5 +1,3 @@
-
-
 ## lm1
 
 library(car)
@@ -9,8 +7,12 @@ library(ggplot2)
 mydata <- read.csv('MidtermData10(HENRY).csv',header = T)
 str(mydata)
 summary(mydata)
+cor(mydata)
 
+#----------------------------------------------------------------------
 ##EDA
+#----------------------------------------------------------------------
+
 par(mfrow=c(1,2))
 boxplot(mydata$x,main='boxplot of x',col=3)
 boxplot(mydata$y,main='boxplot of x',col=4)
@@ -37,10 +39,39 @@ shapiro.test(mydata$y)
 
 
 ggplot(data=mydata,aes(x,y))+geom_point()+    
-      geom_smooth(method = 'lm',col='blue')
+      geom_smooth(method = 'lm',col='blue')+
+      labs(title='y~x')
 
 ggplot(data=mydata,aes(x,y))+geom_point()+
-      geom_smooth(col='red')
+    geom_smooth(col='red',formula = y~x+I(x^2),method = 'lm')+
+    labs(title='y~x+x^2')
+
+ggplot(data=mydata,aes(x,y))+geom_point()+
+    geom_smooth(col='orange',formula = y~x+I(x^2)+I(x^3),method = 'lm')+
+    labs(title='y~x+x^2+x^3')
 
 
+
+#----------------------------------------------------------------------
+# MODEL
+#----------------------------------------------------------------------
+
+lm0 <- lm(y~x,data=mydata)
+lm1 <- lm(y~x-1,data=mydata)
+lm2 <- lm(y~x+I(x^2)-1,data=mydata)
+
+
+
+#----------------------------------------------------------------------
+# MODEL diagnostics
+#----------------------------------------------------------------------
+vif(lm2)
+summary(lm1)
+
+par(mfrow=c(2,2))
+plot(lm1)
+par(mfrow=c(1,1))
+
+
+influencePlot(lm1)
 
