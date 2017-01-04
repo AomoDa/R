c83b9dcb4c030c79c31c1ec7afa1648b8d94dc5c
Aomoda <528451599@qq.com>
2017-01-05 01:15:47 +0800

    Update qr.r

--- R/qr.r
+++ R/qr.r
@@ -1,4 +1,3 @@
-
 library(quantreg)
 library(foreign)
 
@@ -6,7 +5,11 @@ library(foreign)
 x <- read.spss(file = 'data.sav',to.data.frame = T,use.value.labels = T)
 summary(x)
 
+#-----------------------------------
+# part 1
+#-----------------------------------
 
+# 腰围数据
 #Histograms and Q-Q normplot
 par(mfrow=c(1,2))
 hist(x$CDJ3,breaks = 30,probability = T,main='腰围直方图',xlab='腰围')
@@ -21,11 +24,63 @@ data_man <- subset(x,subset = CDA2=='男')
 data_woman <- subset(x,subset = CDA2=='女')
 
 
+# 腰围与性别
+par(mfrow=c(1,2))
+boxplot(CDJ3~CDA2,data=x,ylim=c(50,120),main='性别与腰围箱图')
+plot(density(data_man$CDJ3),main='性别与腰围箱核密度图',xlab='')
+lines(density(data_woman$CDJ3),col='red')
+legend('topright',col=c('black','red'),lty=1,legend=c('男','女'))
+par(mfrow=c(1,1))
+
+# 腰围与性别，年龄
+ggplot(data=x,aes(x=CDAGE,y=CDJ3,col=CDA2))+geom_point()+geom_smooth(method = 'lm',se=F)+lims(y=c(50,120))+labs(title='腰围')
+
+#-----------------------------------
+# part 2
+#-----------------------------------
+
 # 设定分位数
 taus <- c(0.05,  0.25, 0.40 ,0.5, 0.6 ,0.75, 0.8 ,0.90, 0.95)
 
-rq_man<- rq(formula = CDJ3 ~ CDAGE + CDA6+CDA4+CDA5+CDB1+CDC1+CDD1+CDD2+CDD3+CDD5+CDE1+CDE2+CDE4, 
-	tau = taus, data = data_man, method = "fn")
 
-rq_woman<- rq(formula = CDJ3 ~ CDAGE + CDA6+CDA4+CDA5+CDB1+CDC1+CDD1+CDD2+CDD3+CDD5+CDE1+CDE2+CDE4, 
-	tau = taus, data = data_woman, method = "fn")
+
+#与职业
+rq_man<- rq(formula = CDJ3 ~ CDAGE + CDA6,tau = taus, data = data_man, method = "fn")
+rq_woman<-  rq(formula = CDJ3 ~ CDAGE + CDA6,tau = taus, data = data_woman, method = "fn")
+plot(summary(rq_man))
+plot(summary(rq_woman))
+lm_man<- lm(formula = CDJ3 ~ CDAGE + CDA6, data = data_man)
+lm_woman<- lm(formula = CDJ3 ~ CDAGE + CDA6, data = data_woman)
+
+
+
+#与婚姻状况
+rq_man<- rq(formula = CDJ3 ~ CDAGE + CDA5,tau = taus, data = data_man, method = "fn")
+rq_woman<-  rq(formula = CDJ3 ~ CDAGE + CDA5,tau = taus, data = data_woman, method = "fn")
+plot(summary(rq_man))
+plot(summary(rq_woman))
+lm_man<- lm(formula = CDJ3 ~ CDAGE + CDA5, data = data_man)
+lm_woman<- lm(formula = CDJ3 ~ CDAGE + CDA5, data = data_woman)
+
+
+
+
+#与饮食习惯
+rq_man<- rq(formula = CDJ3 ~ CDAGE + CDD3,tau = taus, data = data_man, method = "fn")
+rq_woman<-  rq(formula = CDJ3 ~ CDAGE + CDD3,tau = taus, data = data_woman, method = "fn")
+plot(summary(rq_man))
+plot(summary(rq_woman))
+lm_man<- lm(formula = CDJ3 ~ CDAGE + CDD3, data = data_man)
+lm_woman<- lm(formula = CDJ3 ~ CDAGE + CDD3, data = data_woman)
+
+
+#与锻炼身体情况
+# 无关系
+rq_man<- rq(formula = CDJ3 ~ CDAGE + CDE2,tau = taus, data = data_man, method = "fn")
+rq_woman<-  rq(formula = CDJ3 ~ CDAGE + CDE2,tau = taus, data = data_woman, method = "fn")
+plot(summary(rq_man))
+plot(summary(rq_woman))
+lm_man<- lm(formula = CDJ3 ~ CDAGE + CDE2, data = data_man)
+lm_woman<- lm(formula = CDJ3 ~ CDAGE + CDE2, data = data_woman)
+
+
