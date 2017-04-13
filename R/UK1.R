e98ad62d051d3bda0b3d3f2def93c51d0d521a3f
Aomoda <528451599@qq.com>
2017-04-13 16:35:50 +0800

    Update UK1.R

--- R/UK1.R
+++ R/UK1.R
@@ -1,6 +1,6 @@
-
-
 library(psych)
+library(car)
+library(effects)
 
 uk <- read.csv('ReferendumResults.csv',header = TRUE,stringsAsFactors = FALSE)
 # the proportion of Leave votes
@@ -15,8 +15,8 @@ uk$Age_retirement <- uk$Age_65to74+uk$Age_75to84+uk$Age_85to89+uk$Age_90plus
 uk$C2 <- uk$C2DE - uk$DE
 uk$C1 <- uk$C1C2DE - uk$C2DE
 uk$DE <- uk$DE
-uk$Region <- ifelse(uk$RegionName=='London','London','NO London')
-uk$Area <- ifelse(uk$AreaType=='E09','E09','NO E09')
+uk$Region <- as.factor(ifelse(uk$RegionName=='London','London','NO London'))
+uk$Area <- as.factor(ifelse(uk$AreaType=='E09','E09','NO E09'))
 
 # new data set
 uk_new <- uk[,c(1:10,27:46,49:57)]
@@ -63,7 +63,7 @@ test.data <- train.data[-row.index,]
 par(mfrow=c(2,2))
 boxplot(train.data$LP,col=gray(0.8),main='BoxPlot of LP',xlab='',ylab='LP')
 hist(train.data$LP,probability = T,main='Histogram of LP',xlab='')
-lines(density(train.data$LP),lty=2,col='orange')
+lines(density(train.data$LP),lty=2,col='orange',lwd=2)
 qqPlot(train.data$LP,main='QQPlot of LP',xlab='',ylab='LP')
 plot(train.data$LP,ylim=c(0,1),main='Outline Plot of LP',xlab='',ylab='LP')
 abline(h = mean(train.data$LP),lty=2,lwd=2,col='red')
@@ -84,10 +84,95 @@ summary(aov(LP~AreaType,data = train.data))
 TukeyHSD(aov(LP~RegionName,data = train.data))
 TukeyHSD(aov(LP~AreaType,data = train.data))
 
-
-lm1 <- lm(LP~AreaType+RegionName+RC1+RC2+RC3+RC4+RC5,data=model.data)
-
-lm2 <- lm(LP~RegionName+RC1+RC2+RC3+RC4+RC5,data=model.data)
+pairwise.t.test(x = train.data$LP,g=train.data$RegionName)
+pairwise.t.test(x = train.data$LP,g=train.data$AreaType)
 
 
-lm3 <- lm(LP~Area+Region+RC1+RC2+RC3+RC4+RC5,data=model.data)
+#----
+#ggplot(data=train.data,aes(x=RC1,y=LP,group=Region))+geom_point()+geom_smooth(se=F,aes(col=Region))
+#ggplot(data=train.data,aes(x=RC2,y=LP,group=Region))+geom_point()+geom_smooth(se=F,aes(col=Region))
+#ggplot(data=train.data,aes(x=RC3,y=LP,group=Region))+geom_point()+geom_smooth(se=F,aes(col=Region))
+#ggplot(data=train.data,aes(x=RC4,y=LP,group=Region))+geom_point()+geom_smooth(se=F,aes(col=Region))
+#ggplot(data=train.data,aes(x=RC5,y=LP,group=Region))+geom_point()+geom_smooth(se=F,aes(col=Region))
+#----
+
+###########################################
+##              LM                       ##
+###########################################
+
+lm1 <- lm(LP~RegionName+AreaType+RC1+RC2+RC3+RC4+RC5,data=model.data)
+lm2 <- lm(LP~Region*(RC1+RC2+RC3+RC4+RC5),data=model.data)
+step(lm2)
+lm3 <- lm(formula = LP ~ Region + RC1 + RC2 + RC3 + RC4 + RC5 + Region:RC1 + 
+              Region:RC2 + Region:RC3 + Region:RC4, data = model.data)
+
+lm4 <- lm(formula = LP ~ Region + RC1 + RC2 + RC3  + RC5 + Region:RC1 + 
+                Region:RC3 + Region:RC4, data = model.data)
+
+lm4 <- lm(formula = LP ~ Region + RC1 + RC2 + RC3  + RC5 + Region:RC1 + 
+                Region:RC3 + Region:RC4, data = model.data)
+
+anova(lm1,lm2,lm3,lm4)
+
+# choose lm4
+
+plot(allEffects(lm4))
+
+# Durbin-Watson Test
+dwtest(lm4)
+
+
+
+###########################################
+##              GLM                      ##
+###########################################
+
+glm1 <- glm(cbind(Leave,NVotes-Leave)~Region + RC1 + RC2 + RC3  + RC5 + Region:RC1 + Region:RC3 + Region:RC4,
+	data=model.data,family = quasibinomial(link='logit'))
+
+
+plot(allEffects(glm1))
+
+
+s <- function(model,data){
+	dispersion <- summary(model)$dispersion
+	pred <- predict(model,newdata = data,se.fit = TRUE,type = 'response')
+	p <- pred$fit
+	if(is.null(dispersion)){
+      sigma <- sqrt(pred$se.fit^2 + pred$residual.scale^2)} else{
+		sigma <- sqrt((pred$se.fit^2 + dispersion* p* (1-p)/data$NVotes))
+	}
+    s_value <- sum( log(sigma) + (data$LP-pred$fit)^2 / (2*sigma^2) )
+    return(s_value)
+}
+
+s(model = lm4,data = model.data)
+s(model = lm4,data = test.data)
+s(model = glm1,data = model.data)
+s(model = glm1,data = test.data)
+
+
+cv.s <- function(data,k=5,seeds=2017){
+    set.seed(seeds)
+    ind <- sample(x=k,size = nrow(data),replace = T)
+    ans <- data.frame(lm.train.s=numeric(k),lm.test.s=numeric(k),
+    	              glm.train.s=numeric(k),glm.test.s=numeric(k)) 
+    for (i in 1:k) {
+        a <- data[ind!=i,]
+        b <- data[ind==i,]
+        lm.test <- lm(formula = LP ~ Region + RC1 + RC2 + RC3  + RC5 + Region:RC1 + 
+                                Region:RC3 + Region:RC4, data = a)
+        glm.test <- glm(cbind(Leave,NVotes-Leave)~Region + RC1 + RC2 + RC3  + RC5 +
+                        Region:RC1 + Region:RC3 + Region:RC4,
+	                    data=a,family = quasibinomial())
+        ans[i,1] <- s(lm4,a)
+        ans[i,2] <- s(lm4,b)
+        ans[i,3] <- s(glm1,a)
+        ans[i,4] <- s(glm1,b)
+    }
+ return(ans)
+}
+
+cv.s(data = train.data,k = 5,seeds = 100)
+
+
