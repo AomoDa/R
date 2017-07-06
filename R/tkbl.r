85d6bfe1353d9d0c3c5beaafc255b4556495e05b
Aomoda <528451599@qq.com>
2017-07-06 23:06:06 +0800

    Create tkbl.r

---
+++ R/tkbl.r
@@ -0,0 +1,19 @@
+myf <- function(init,vec,cost_yesterday=1/3,bound=2,plot=TRUE) {
+	cost_today <- 1- cost_yesterday
+	n <- length(vec)
+	rt <- numeric(n)
+    # loop compute today value
+	for (i in 1:n) {
+		if(i==1){
+			a <- init
+		}else{
+			a <- rt[i-1]
+		}
+		rt[i] <- a * cost_yesterday + vec[i] * cost_today
+	}
+	if(plot){
+		plot(1:n,rt,type='b',pch=16,xlab='',ylab='')
+		abline(h=bound,lty=2,lwd=1.5,col='red')		
+	}
+	return(match(TRUE,rt<2))
+}
